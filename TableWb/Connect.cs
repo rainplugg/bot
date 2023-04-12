213cf48b159439a42d93bdfb873a75364574fa45
rainplugg <130517913+rainplugg@users.noreply.github.com>
2023-04-13 02:57:10 +0500

    Add files via upload

---
+++ TableWb/Connect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Text;
+
+namespace TableWb
+{
+   public class Connect
+   {
+      public static SQLiteDataReader Query(string str)
+      {
+         SQLiteConnection SQLiteConnection = new SQLiteConnection("Data Source=|DataDirectory|wb.db");
+         SQLiteCommand SQLiteCommand = new SQLiteCommand(str, SQLiteConnection);
+         try {
+            SQLiteConnection.Open();
+            SQLiteDataReader reader = SQLiteCommand.ExecuteReader();
+            return reader;
+         } catch { return null; }
+      }
+
+      public static void LoadMessage(List<Message> data)
+      {
+         try {
+            data.Clear();
+            SQLiteDataReader query = Query("select * from `Message`;");
+            if (query != null) {
+               while (query.Read()) {
+                  data.Add(new Message(
+                     query.GetValue(0).ToString(),
+                     query.GetValue(1).ToString()
+                  ));
+               }
+            }
+         } catch { }
+      }
+   }
+}
