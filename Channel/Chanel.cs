3ea037325ab9dd3cceb17509365e20bfb80aa147
rainplugg <130517913+rainplugg@users.noreply.github.com>
2023-04-12 05:34:42 +0500

    Add files via upload

---
+++ Channel/Chanel.cs
@@ -0,0 +1,15 @@
+﻿namespace Chanel
+{
+   public class Chanel
+   {
+      public long id { get; set; }
+      public string topic { get; set; }
+      public string title { get; set; }
+      public Chanel(long id, string topic, string title)
+      {
+         this.id = id;
+         this.topic = topic;
+         this.title = title;
+      }
+   }
+}
---
+++ Channel/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chanel
+{
+   public class Post
+   {
+      public string id { get; set; }
+      public string reply { get; set; }
+      public Post(string id, string reply)
+      {
+         this.id = id;
+         this.reply = reply;
+      }
+
+      internal object Last()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
