540386a3a83541d1a7f49e7ce3807e16d875705b
rainplugg <130517913+rainplugg@users.noreply.github.com>
2023-06-17 22:07:55 +0500

    Add files via upload

---
+++ Course/Classes/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Course.Classes
+{
+   public class User
+   {
+      public long Id { get; set; }
+      public string Message { get; set; }
+      public string Sub { get; set; }
+      public int Id_list { get; set; }
+      public int Balance { get; set; }
+      public long Id_invited { get; set; }
+      public User(long id, string message, string sub, int id_list, int balance, long id_invited)
+      {
+         Id = id;
+         Message = message;
+         Sub = sub;
+         Id_list = id_list;
+         Balance = balance;
+         Id_invited = id_invited;
+      }
+   }
+}
