19f2cb672b1db59fafe62bf2585e7c831bebea57
rainplugg <130517913+rainplugg@users.noreply.github.com>
2023-06-23 22:54:13 +0500

    Add files via upload

---
+++ Course/Classes/Refer.cs
@@ -0,0 +1,13 @@
+﻿namespace Course.Classes
+{
+   public class Refer
+   {
+      public long Id { get; set; }
+      public int Count { get; set; }
+      public Refer(long id, int count)
+      {
+         Id = id;
+         Count = count;
+      }
+   }
+}
